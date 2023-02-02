2dae6764b78e8b4b45782c784cd43115b5e129c2
RAKESH KUMAR <rakeshkumar756413936@gmail.com>
2023-02-02 10:39:12 +0530

    completd

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # enacton
+
+//=================input==================
+
+
+//writing Fast Tests Against Enterprise Raills
+//overding it is python
+//Lua for the masses
+//Ruby Errors from Mismatched Gem version
+//Lunch
+//Ruby on Rails
+//Common Ruby Errors
+//pair programming vs Noise
+//programming in  the Boondocks of stattle
+//user interface CSS in Rails Apps
+//communication our distance
+//Rails magic
+//woah
+//Sit Down and write
+//Lunch
+//Accounting Diven Development
+//clojure Ate scale (on any project)
+//A world without hackerNews 
+//Ruby on railslanguge App maintanance
+//Rails for python Developer lightning
+//networking Event
+
+
+===============code flow  summar=======================
+
+1 implemented time scheduling of event from  npm package node-schedule
+2 The required  schedule event is shown is console part
+3 The main logic file is tarck.js 
+4 To schedule the event i have used schedulejob function from node schedule and to close the event job.close() is used.
+
---
+++ src/tracks/track.js
@@ -0,0 +1,99 @@
+
+
+
+const schedule=require('node-schedule')
+
+//====================track-1==================================
+
+const task1 = schedule.scheduleJob(' * 00 09 * * *', function(){
+    console.log('writing Fast Tests Against Enterprise Raills');
+    task1.cancel()
+  });
+const task2 = schedule.scheduleJob('* 00 10 * * *', function(){
+    console.log('overding it is python');
+    task2.cancel()
+  });
+const task3 = schedule.scheduleJob('* 45 10 * * *', function(){
+    console.log('Lua for the masses');
+    task3.cancel()
+  });
+  const task4 = schedule.scheduleJob('* 15 11 * * *', function(){
+    console.log('Ruby Errors from Mismatched Gem version');
+    task4.cancel()
+  });
+  const task5 = schedule.scheduleJob('* 00 12 * * *', function(){
+    console.log('Lunch');
+    task5.cancel()
+  });
+  const task6 = schedule.scheduleJob('* 00 13 * * *', function(){
+    console.log('Ruby on Rails');
+    task6.cancel()
+  });
+  const task7 = schedule.scheduleJob('* 00 14 * * *', function(){
+    console.log('Common Ruby Errors');
+    task7.cancel()
+  });
+  const task8 = schedule.scheduleJob('* 45 14 * * *', function(){
+    console.log('pair programming vs Noise');
+    task8.cancel()
+  });
+  const task9 = schedule.scheduleJob('* 30 15 * * *', function(){
+    console.log('programming in  the Boondocks of stattle');
+    task9.cancel()
+  });
+  const task10 = schedule.scheduleJob('* 30 16 * * *', function(){
+    console.log('user interface CSS in Rails Apps');
+    task10.cancel()
+  });
+  const task11 = schedule.scheduleJob('* 00 17 * * *', function(){
+    console.log('Networking Events');
+    task11.cancel()
+  });
+
+  //====================track-2===================================
+
+
+  const task12 = schedule.scheduleJob('* 00 09 * * *', function(){
+    console.log('communication our distance');
+    task12.cancel()
+  });
+  const task13 = schedule.scheduleJob('* 00 10 * * *', function(){
+    console.log('Rails magic');
+    task13.cancel()
+  });
+  const task14 = schedule.scheduleJob('* 00 11 * * *', function(){
+    console.log('woah');
+    task14.cancel()
+  });
+  const task15 = schedule.scheduleJob('* 30 11 * *', function(){
+    console.log('Sit Down and write');
+    task15.cancel()
+  });
+  const task16 = schedule.scheduleJob('* 00 12 * * *', function(){
+    console.log('Lunch');
+    task16.cancel()
+  });
+  const task17 = schedule.scheduleJob('* 00 13 * * *', function(){
+    console.log('Accounting Diven Development');
+    task17.cancel()
+  });
+  const task18 = schedule.scheduleJob('* 45 13 * * *', function(){
+    console.log('clojure Ate scale (on any project)');
+    task18.cancel()
+  });
+  const task19 = schedule.scheduleJob('* 30 14 * * *', function(){
+    console.log('A world without hackerNews ');
+    task19.cancel()
+  });
+  const task20 = schedule.scheduleJob('* 00 15 * * *', function(){
+    console.log('Ruby on railslanguge App maintanance');
+    task20.cancel()
+  });
+  const task21 = schedule.scheduleJob('* 00 16 * * *' , function(){
+    console.log('Rails for python Developer lightning');
+    task21.cancel()
+  });
+  const task22 = schedule.scheduleJob('* 00 17 * * *', function(){
+    console.log('networking Event');
+    task22.cancel()
+  });
